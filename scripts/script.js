4349b717e8cc0d951f372a521dd2fc5e2ec698c1
Marengd <94312410+Marengd@users.noreply.github.com>
2021-12-10 14:37:27 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,146 @@
-// JavaScript Document
 
-var menu = document.querySelector("header nav");
-var submenu = document.querySelector("header nav > nav");
 
+
+
+
+
+
+
+
+/* JavaScript
+
+
+
+
+
+/* ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
+/* ░░░░░░░░░░░░░░░  Hamburger menu  ░░░░░░░░░░░░░░░░ */
+/* ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
+
+
+var vldgMenu = document.querySelector("header nav");
 var hamMenu = document.querySelector("nav ul");
 
-var hamButton = document.querySelector("nav button:last-of-type");
-var icoon = document.querySelector("nav button:last-of-type::before");
+var knpMenu = document.querySelector("nav button:last-of-type");
 
 
+// Openen en sluiten menu
 
 
-// Click on menu Button
-
-hamButton.addEventListener("click", toggleHamburgerMenu);
-
-function toggleHamburgerMenu(){
-    hamMenu.classList.toggle("visible");
-  //submenu.classList.toggle("zien");
-    menu.classList.toggle("uitgeklapt");
+function schakelenMenu(){
+  hamMenu.classList.toggle("weergeven");
+  vldgMenu.classList.toggle("uitschuiven");
 }
 
+  knpMenu.addEventListener("click", schakelenMenu);
 
 
-var achtergrond = document.querySelector("main section:nth-of-type(4) article:last-of-type");
-var kleineKop = document.querySelector("main section:nth-of-type(4) article:last-of-type h4");
+
+
+
+/* ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
+/* ░░░░░░░░░░░░░░░░░░░  Hover 1  ░░░░░░░░░░░░░░░░░░░ */
+/* ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
+
+var mldie = document.querySelector("audio");
+
+var geleblok = document.querySelector("main section:nth-of-type(4) article:last-of-type");
+
+var klneKop = document.querySelector("main section:nth-of-type(4) article:last-of-type h4");
 var twix = document.querySelector("main section:nth-of-type(4) article:last-of-type img");
-var groteKop = document.querySelector("main section:nth-of-type(4) article:last-of-type h3");
+var grteKop = document.querySelector("main section:nth-of-type(4) article:last-of-type h3");
+var lnkrknop = document.querySelector("main section:nth-of-type(4) article:last-of-type a:first-of-type");
+var rchtrknop = document.querySelector("main section:nth-of-type(4) article:last-of-type a:last-of-type");
 
-function lichtOp() {
-    achtergrond.classList.add('achtergrond-opvullen');
-    kleineKop.classList.add('kleineKop-oplichten');
-    twix.classList.add('vergroot');
-    groteKop.classList.add('groteKop-oplichten');
+
+// Met hover
+
+
+function oplichten() {
+    mldie.volume = 0.1;
+    mldie.play();
+
+    geleblok.classList.add('opvullen-achtergrond');
+
+    klneKop.classList.add('oplichten-kleine');
+    twix.classList.add('vergroten');
+    grteKop.classList.add('oplichten-grote');
+    lnkrknop.classList.add('oplichten');
+    rchtrknop.classList.add('oplichten');
   }
 
+  geleblok.addEventListener('mouseover', oplichten);
+  
 
-  // EventListeners
-  achtergrond.addEventListener('mouseover', lichtOp)
-  
-  
-  // Dom Manipulatie
-  function doof() {
-    achtergrond.classList.remove('achtergrond-opvullen');
-    kleineKop.classList.remove('kleineKop-oplichten');
-    twix.classList.remove('vergroot');
-    groteKop.classList.remove('groteKop-oplichten');
+  // Zonder hover
+
+
+  function uitdoven() {
+    mldie.pause();
+    mldie.currentTime = 0;
+
+    geleblok.classList.remove('opvullen-achtergrond');
+
+    klneKop.classList.remove('oplichten-kleine');
+    twix.classList.remove('vergroten');
+    grteKop.classList.remove('oplichten-grote');
+    lnkrknop.classList.remove('oplichten');
+    rchtrknop.classList.remove('oplichten');
   }
-  // EventListeners
-  achtergrond.addEventListener('mouseout' , doof)
+
+  geleblok.addEventListener('mouseout' , uitdoven);
+
+
+
+
+
+
+
+
+
+
+/* ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
+/* ░░░░░░░░░░░░░░░░░░  Carousel  ░░░░░░░░░░░░░░░░░░░ */
+/* ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
+
+
+  var carousel = document.getElementById('carousel');
+  var cards = document.querySelectorAll('.card');
+  var tray = document.getElementById('tray');
+  var indicator = document.getElementById('indicator');
+  var info = {
+    numCards: cards.length
+  };
+  
+  var getDimensions = function() {
+    info.container_width = carousel.clientWidth;
+    info.card_width = carousel.firstElementChild.clientWidth;
+    info.tray_width = tray.clientWidth;
+  }
+  
+  var moveIndicator = function(timestamp) {
+    var amount_inview = info.container_width / (info.card_width * info.numCards); // < 1
+    var tray_scale = info.tray_width / info.container_width;
+  
+    var indicator_width = info.tray_width * amount_inview;
+    var indicator_offset = (info.scroll_left * amount_inview) * tray_scale;
+  
+    indicator.style.width = indicator_width + 'px';
+    indicator.style.left = indicator_offset + 'px';
+  
+    requestAnimationFrame(moveIndicator);
+  }
+  
+  var onScroll = function() {
+    info.scroll_left = carousel.scrollLeft;
+  }
+  
+  // initialize
+  getDimensions();
+  carousel.addEventListener('scroll', onScroll);
+  requestAnimationFrame(moveIndicator);
+
+
+
+
+
